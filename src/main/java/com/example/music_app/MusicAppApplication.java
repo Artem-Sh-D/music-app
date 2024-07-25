6316d8271ed686cebf27f41e75463613bd7005cd
Artem <bakol6000@gmail.com>
2024-07-25 23:18:54 +0300

    music-app create

--- src/main/java/com/example/music_app/MusicAppApplication.java
+++ src/main/java/com/example/music_app/MusicAppApplication.java
@@ -3,7 +3,7 @@ package com.example.music_app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication__
+@SpringBootApplication
 public class MusicAppApplication {
 
 	public static void main(String[] args) {
